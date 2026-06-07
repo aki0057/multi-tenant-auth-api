e8c0c1da8eb003d889f284ce6653ba4a53d3b015
aki <y.0803aki1999@gmail.com>
2026-06-07 16:16:29 +0900

    Initial commit

---
+++ src/main/java/io/github/aki0057/multitenant/auth/MultiTenantAuthApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.aki0057.multitenant.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenantAuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/aki0057/multitenant/auth/MultiTenantAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aki0057.multitenant.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
